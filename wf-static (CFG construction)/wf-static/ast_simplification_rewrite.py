69e7f6cf627f916cc1cd905a43d7887e026e03e6
mike-jones <michaeldeitch04@gmail.com>
2025-11-19 09:19:48 -0700

    Partial Rewrite of ast_simplification -- For loops
    Part of ast_simplification deals with transforming for-loops to
    while loops. This has be rewritten for C, using pycparser.

---
+++ wf-static (CFG construction)/wf-static/ast_simplification_rewrite.py
@@ -0,0 +1,44 @@
+import copy
+
+from pycparser import c_ast, parse_file
+from pycparser.c_ast import For
+from pycparser.c_ast import While
+from pycparser.c_ast import DoWhile
+
+class CompoundVisitor(c_ast.NodeVisitor):
+    def visit_Compound(self, node):
+        if hasattr(node, 'block_items'):
+            for index, sub_node in enumerate(node.block_items): #https://stackoverflow.com/questions/19162285/get-index-in-the-list-of-objects-by-attribute-in-python
+                if isinstance(sub_node, For):
+                    print(f"[Coord] -> [File = {sub_node.coord.file}] [Line = {sub_node.coord.line}]\n")
+                    coord = sub_node.coord
+                    stmt = sub_node.stmt
+                    cond = sub_node.cond
+
+                    replacement_while = While(cond, stmt, coord)
+                    replacement_while.stmt.block_items.append(sub_node.next)
+
+                    decls = node.block_items[index].init.decls
+                    for decl in decls:
+                        node.block_items.insert(index, decl)  #todo might need to change for when the init has multiple decls?
+                        index = index + 1
+
+                    node.block_items[index] = replacement_while
+
+
+class DoWhileVisitor(c_ast.NodeVisitor):
+    def visit_While(self, node):
+        print(f"[Coord] -> [File = {node.coord.file}]\n")
+
+
+
+def run(ast):
+    #Transform all for-loops to while loops
+    v = CompoundVisitor()
+    v.visit(ast)
+
+    stop = 1
+
+    ast.show()
+    #simplify strings in program
+    #ASk about he utlity of this
