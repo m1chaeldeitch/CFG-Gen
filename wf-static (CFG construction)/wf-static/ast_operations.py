df19543c8013d09daf7ff41908762d62b74c4cb4
mike-jones <michaeldeitch04@gmail.com>
2026-02-25 10:01:23 -0700

    Made searc function more general
    Also created a function utilizing this more general search function that
    finds all nodes accessible from the inputted "root" cfg (in my cases, main),
    and returns a list of the nodes.

--- wf-static (CFG construction)/wf-static/ast_operations.py
+++ wf-static (CFG construction)/wf-static/ast_operations.py
@@ -4,17 +4,18 @@ from pycparser.c_ast import NodeVisitor
 from pycparser.c_ast import FuncCall
 
 class FuncCallVisitor(c_ast.NodeVisitor):
-    def __init__(self):
+    def __init__(self, desired_func_name):
         self.found = False
         self.names = []
+        self.desired_func = desired_func_name
 
     def visit_FuncCall(self, node):
-        if node.name.name == 'pthread_create': #Todo account for not just .name.name but expressions too?
+        if node.name.name == self.desired_func: #Todo account for not just .name.name but expressions too?
             self.found = True
         else:
             self.names.append(node.name.name)
 
-def find_pthread(ast):
-    v = FuncCallVisitor()
+def find_func(ast, desired_func_name):
+    v = FuncCallVisitor(desired_func_name)
     v.visit(ast)
     return v
--- wf-static (CFG construction)/wf-static/graph_utils.py
+++ wf-static (CFG construction)/wf-static/graph_utils.py
@@ -119,9 +119,12 @@ def validate_thread_creation(main_cfg):
     pass
     return thread_create_nodes
 
-def validate_thread_creation2(cfg, all_cfgs, all_func_names):
-    thread_create_nodes = []
-    thread_create_map = {}
+'''
+General Function to use when looking for any arbitrary function accessible from the "cfg" argument
+'''
+def find_all_nodes_for_func(cfg, all_cfgs, all_func_names, desired_func):
+    func_node = []
+    func_call_map = {}
     #thread_create_nodes["func_name"] = [node1, node2] todo Maybe adapt to this, not necessary under current plan
     funcs_called = []
 
@@ -138,27 +141,34 @@ def validate_thread_creation2(cfg, all_cfgs, all_func_names):
         # Look for the pthread_create function being called
         for statement in basic_block._statements:
             if statement is not None:
-                statement_search = ast_operations.find_pthread(statement)
+                statement_search = ast_operations.find_func(statement, desired_func)
 
                 #If the pthread_create() is found, mark this as true and then keep track of it
                 #If false, add the name to the list of functions that are accessible by the current function
                 if statement_search.found is True:
-                    thread_create_nodes.append(cfg.nodes[i])
+                    func_node.append(cfg.nodes[i])
                 elif statement_search.found is False:
                     for name in statement_search.names:
                         funcs_called.append(name)
 
         #Associate all thread creation nodes to the current function name todo maybe implement the mapping later
-        if thread_create_nodes:
-            thread_create_map[cfg.nodes[0]['label'].split(" ")[1].strip("()")] = thread_create_nodes
+        if func_node:
+            func_call_map[cfg.nodes[0]['label'].split(" ")[1].strip("()")] = func_node
 
 
     ## (2) Look for calls to pthread_create() within the functions that the current function called
     for func in funcs_called:
         if func in all_func_names:
             curr_cfg = all_cfgs[func]
-            additional_nodes = validate_thread_creation2(curr_cfg, all_cfgs, all_func_names)
-            thread_create_nodes.extend(additional_nodes)
+            additional_nodes = find_all_nodes_for_func(curr_cfg, all_cfgs, all_func_names)
+            func_node.extend(additional_nodes)
             funcs_called.remove(func)
 
-    return thread_create_nodes
+    return func_node
+
+'''
+Example function that finds all nodes that make a call to pthread_create()
+'''
+def find_pthread_create(main_cfg, all_cfgs, all_func_names):
+    return find_all_nodes_for_func(main_cfg, all_cfgs, all_func_names, "pthread_create")
+
